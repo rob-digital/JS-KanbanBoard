ac6e0fcefa062f286801e0d13c218c37752a3209
rob-digital <robert.roksela@gmail.com>
2021-09-08 12:55:03 +0100

    project completed

--- js/kanban.js
+++ js/kanban.js
@@ -16,7 +16,51 @@ add_btn.addEventListener('click', () => {
 });
 
 const create_item = () => {
+  let item = document.createElement('div')
+  item.classList.add('item')
+  item.id = 'item-' + order
+  item.draggable = true
+
+  item.addEventListener('dragstart', event => {
+    event.dataTransfer.setData('text', event.target.id)
+  })
+
+  let input = document.createElement('input')
+  item.appendChild(input)
+
+  let save_btn = document.createElement('button')
+  save_btn.innerHTML = 'Save'
+
+  save_btn.addEventListener('click', () => {
+    error.innerHTML = ''
+    if (input.value !== '') {
+      order += 1
+      item.innerHTML = input.value
+      adding = false
+    } else {
+      error.innerHTML = message
+    }
+  })
+
+  
+  item.addEventListener('dragend', event => {
+    event.dataTransfer.clearData()
+  })
+  
+  item.appendChild(save_btn)
+
+  return item
+
 };
 
 document.querySelectorAll('.drop').forEach(element => {
+  
+  element.addEventListener('drop', event => {
+    event.preventDefault()
+    const id = event.dataTransfer.getData('text')
+    event.target.appendChild(document.getElementById(id))
+  })
+  element.addEventListener('dragover', event => {
+    event.preventDefault()
+  })
 });
